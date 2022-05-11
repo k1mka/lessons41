5e880ec57a3bb154149872fe619152301f3032ec
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-05-12 01:44:08 +0300

    Add project files.

---
+++ lessons41/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace lessons41
+{
+    internal class Program
+    {
+
+        static int SumArray(int[] myArray, int i = 0)
+        {
+
+
+            if (i >= myArray.Length)
+                return 0;
+
+            int result = SumArray(myArray, i + 1);
+            return myArray[i] + result;
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 4, 5 };
+
+            int result = SumArray(myArray);
+        }
+    }
+}
